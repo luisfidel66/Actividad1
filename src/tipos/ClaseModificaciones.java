b2b255a6b1d002f94d84e5f74e905ab274c73857
Fidel <Fidel@DESKTOP-6HUHE9F>
2021-01-31 19:03:28 +0100

    Sexto Cambio

--- src/tipos/ClaseModificaciones.java
+++ src/tipos/ClaseModificaciones.java
@@ -5,6 +5,7 @@ public class ClaseModificaciones {
 	public static void main(String[] args) {
 		System.out.println("Hello again");
 		System.out.println("Nueva modificacion");
+		System.out.println("Hasta la proxima");
 
 	}
 
